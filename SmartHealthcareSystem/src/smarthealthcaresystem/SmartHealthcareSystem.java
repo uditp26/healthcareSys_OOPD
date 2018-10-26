df0755076393f387e2af497023f55c2186d22666
Udit Pant <udit18049@iiitd.ac.in>
2018-10-27 03:30:25 +0530

    HomePage v1.0

--- SmartHealthcareSystem/src/smarthealthcaresystem/SmartHealthcareSystem.java
+++ SmartHealthcareSystem/src/smarthealthcaresystem/SmartHealthcareSystem.java
@@ -5,9 +5,12 @@
  */
 package smarthealthcaresystem;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -20,23 +23,10 @@ import javafx.stage.Stage;
 public class SmartHealthcareSystem extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("SHSView.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setTitle("Smart Healthcare System");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
